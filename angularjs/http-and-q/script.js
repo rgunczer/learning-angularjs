18cad309c13b39c3a91666a1ee9fe6210f03c7e8
NITRO15\flanker <rgunczer@gmail.com>
2017-11-04 19:55:30 +0100

    $http loading indicator

--- angularjs/http-and-q/script.js
+++ angularjs/http-and-q/script.js
@@ -1,9 +1,32 @@
 'use strict';
 
 angular.module('myApp', [])
+    .run(['$http', '$log', '$rootScope', function($http, $log, $rootScope) {
+        $rootScope.$watch(function () {
+            var pending = $http.pendingRequests.length > 0;
+            $log.info('WATCHER >>>>>>>>>>>>>>>>>>>>> pending count: ' + $http.pendingRequests.length);
+            return pending;
+        }, function(newValue, oldValue) {
+            $log.info('BROADCASTING');
+            var urls = $http.pendingRequests.map(function(x) {
+                return x.url;
+            });
+            $log.info('pending requests: ' + urls);
+            $rootScope.$broadcast('$httpEvent', { urls: urls });
+        });
+    }])
     .controller('myController', ['$scope', '$log', 'myData', function ($scope, $log, myData) {
         $scope.text = "If you can read this then AngularJS is working fine";
         $log.info('hello from myController');
+        $scope.urlsToWatch = ['data/countries.json', 'data/colors.json'];
+        $scope.submit = function() {
+            // myData.load()
+            // .then(function(result) {
+            //     $log.info('submit then: ' + result);
+            // });
+            myData.loadSomethingOther();
+        };
+
         myData.load()
             .then(function (result) {
                 $log.info('all data is received in myController');
@@ -20,17 +43,82 @@ angular.module('myApp', [])
     .controller('myOtherController', ['$scope', '$log', 'myData', function ($scope, $log, myData) {
         $scope.text = "Hello from myOtherController If you can read this then AngularJS is working fine";
         $log.info('hello from myOtherController');
+        $scope.urlsToWatch = ['*'];
         myData.load()
             .then(function(result) {
                 $log.info('all data is received in myOtherController');
             }, function (error) {
                 $log.info('error loading data in myOtherController ' + angular.toJson(error));
             });
+
+        $scope.countries = myData.countries;
+        $scope.colors = myData.colors;
+    }])
+    .directive('myLoading', [function() {
+        return {
+            restrict: 'E',
+            scope: {
+                urls: '='
+            },
+            template: '<div>loading status: {{loading}}</div>',
+            controller: ['$scope', '$http', '$log', function($scope, $http, $log) {
+                $scope.loading = false;
+
+                $scope.$on('$httpEvent', function(event, data) {
+                    $log.info('Got httpEvent in: ' + $scope.$id);
+                    if ($scope.urls.length === 1 && $scope.urls[0] === '*') {
+                        $scope.loading = data.urls.length > 0;
+                    } else {
+                        var pendingCount = 0;
+                        for(var i = 0; i < data.urls.length; ++i) {
+                            for(var j = 0; j < $scope.urls.length; ++j) {
+                                if (data.urls[i].indexOf($scope.urls[j]) !== -1) {
+                                    ++pendingCount;
+                                }
+                            }
+                        }
+
+                        $log.info('urls: ' + $scope.urls);
+                        $scope.loading = pendingCount > 0;
+                    }
+                });
+
+                // $scope.$watch(function () {
+                //     $log.info('WATCHER>>>>>>>>>>>>>>>>>>>>>');
+                //     var pendingCount = 0;
+                //     for(var i = 0; i < $http.pendingRequests.length; ++i) {
+                //         for(var j = 0; j < $scope.urls.length; ++j) {
+                //             if ($http.pendingRequests[i].url.indexOf($scope.urls[j]) !== -1) {
+                //                 ++pendingCount;
+                //             }
+                //         }
+                //     }
+                //     $log.info('urls: ' + $scope.urls);
+                //     //return $http.pendingRequests.length > 0;
+                //     return pendingCount;
+                // }, function(hasPending) {
+                //     if (hasPending) {
+                //         $scope.loading = true;
+                //     } else {
+                //         $scope.loading = false;
+                //     }
+                // });
+
+            }]
+        }
     }])
     .service('myData', ['$http', '$log', '$q', function ($http, $log, $q) {
-        $log.info('myData BEGIN...');
+        $log.info('myData Service instantiated...');
 
         var deferred = $q.defer();
+
+        this.getUrlCountries = function() {
+            return 'http://127.0.0.1:8080/data/countries.json';
+        };
+
+        this.getUrlColors = function() {
+            return 'http://127.0.0.1:8080/data/colors.json';
+        }
 
         var countriesPromise;
         var colorsPromise;
@@ -39,53 +127,40 @@ angular.module('myApp', [])
         this.colors = [];
         var that = this;
 
-        function getCountries() {
-            var prefix = 'countries ';
-            var url = 'http://127.0.0.1:8080/data/countries.json';
-            return $http.get(url);
-                // .then(function (response){
-                //     $log.info(prefix + 'data received...');
-                //     $log.info(response.data);
-                //     that.countries.length = 0;
-                //     response.data.forEach(function(element) {
-                //         that.countries.push(element);
-                //     });
-                // }, function (error) {
-                //     $log.info(prefix + 'error unable to receive data...' + angular.toJson(error));
-                // })
-                // .finally(function() {
-                //     $log.info(prefix + 'data load finally block...');
-                // });
+        function processCountries(data){
+            that.countries.length = 0;
+            data.forEach(function(element) {
+                that.countries.push(element);
+            });
         }
 
-        function getColors() {
-            var prefix = 'colors';
-            var url = 'http://127.0.0.1:8080/data/colors1.json';
-            return $http.get(url);
-                // .then(function (response) {
-                //     $log.info(prefix + 'data received...');
-                //     $log.info(response.data);
-                // }, function(error) {
-                //     $log.info(prefix + 'error unable to receie data...' + angular.toJson(error));
-                // })
-                // .finally(function() {
-
-                // });
+        function processColors(data) {
+            that.colors.length = 0;
+            data.forEach(function(element) {
+                that.colors.push(element);
+            });
         }
 
         this.load = function() {
             $log.info('myData load...');
             if (angular.isUndefined(countriesPromise)) {
-                countriesPromise = getCountries();
+                countriesPromise = $http.get(this.getUrlCountries());
             }
             if (angular.isUndefined(colorsPromise)) {
-                colorsPromise = getColors();
+                colorsPromise = $http.get(this.getUrlColors());
+            }
+
+            if (this.colors.length > 0 && this.countries.length > 0) {
+                return $q.when('ok');
             }
 
             $q.all([colorsPromise, countriesPromise])
                 .then(function (results) {
                     $log.info(angular.toJson(results));
-                    deferred.resolve(results);
+                    processColors(results[0].data);
+                    processCountries(results[1].data);
+
+                    deferred.resolve('ok');
                 }, function (errors) {
                     deferred.reject(errors);
                 })
@@ -93,6 +168,10 @@ angular.module('myApp', [])
 
                 });
             return deferred.promise;
+        };
+
+        this.loadSomethingOther = function() {
+            $http.get('http://127.0.0.1:8080/data/colors1.json');
         }
 
     }]);
