61ed946adf80b0bbe107b18c813a0fdf2fdb33e9
NITRO15\flanker <rgunczer@gmail.com>
2017-11-23 21:23:30 +0100

    requirejs tutorial

---
+++ requirejs/config.js
@@ -0,0 +1,12 @@
+require.config({
+    baseUrl: 'js',
+    paths: {
+        angular: 'angular.min',
+        extCore: 'ext-core',
+        jquery: [
+            'http://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.2.1',
+            'jquery.min'
+        ],
+        mootools: 'mootools.min'
+    }
+});
---
+++ requirejs/js/customScripts/jqueryFile.js
@@ -0,0 +1,11 @@
+alert('sff');
+
+// define(['', '', '', ''], function() {
+
+// });
+
+define(['jquery'], function($) {
+
+    $('body').html('Hello from jquery');
+
+});
