75582626baa6171024def1d2f80570016232249d
NITRO15\flanker <rgunczer@gmail.com>
2018-01-20 18:14:30 +0100

    angularjs skeleton update

--- angularjs/skeleton/script.js
+++ angularjs/skeleton/script.js
@@ -2,4 +2,4 @@ angular.module('myApp', [])
 .controller('myController', ['$scope', '$log', function ($scope, $log) {
     $scope.text = "If you can read this then AngularJS is working fine";
     $log.info('hello from myController');
-}])
+}]);
