0dc6a21f59b4b73e0914d901c3627dc174f82925
NITRO15\flanker <rgunczer@gmail.com>
2018-02-16 23:17:25 +0100

    eslint mods

--- angularjs/http-legacy/script.js
+++ angularjs/http-legacy/script.js
@@ -1,22 +1,22 @@
 angular.module('myApp', [])
 
-.config(function($httpProvider) {
-    console.log('inside config fn');
-    $httpProvider.useLegacyPromiseExtensions(false);
+    .config(function($httpProvider) {
+        console.log('inside config fn');
+        $httpProvider.useLegacyPromiseExtensions(false);
 
     // $http .success, $error returns
     ///angular.js:13642 Error: [$http:legacy] The method `success` on the promise returned from `$http` has been disabled.
-})
-
-.controller('myController', function ($scope, $log, $http) {
-    $scope.text = "If you can read this then AngularJS is working fine";
-    $log.info('hello from myController');
-
-    $http.get('data.json')
-    .success(function(response) {
-        $log.info(angular.toJson(response));
     })
-    .error(function(error) {
-        $log.info(angular.toJson(error));
+
+    .controller('myController', function ($scope, $log, $http) {
+        $scope.text = "If you can read this then AngularJS is working fine";
+        $log.info('hello from myController');
+
+        $http.get('data.json')
+            .success(function(response) {
+                $log.info(angular.toJson(response));
+            })
+            .error(function(error) {
+                $log.info(angular.toJson(error));
+            })
     })
-})
--- angularjs/skeleton/app.js
+++ angularjs/skeleton/app.js
@@ -1,4 +1,5 @@
-angular.module('myApp', [])
+angular.module('App', [])
+
     .controller('myController', function ($scope, $log) {
         $scope.text = 'If you can read this then AngularJS is working fine';
         $log.info('hello from myController');
