b2366c5b6eb7bcfb1a2a0103ac7b441377748fcd
NITRO15\flanker <rgunczer@gmail.com>
2018-02-19 21:51:42 +0100

    add new component

--- angular/angular2-essential-training/app/app.component.ts
+++ angular/angular2-essential-training/app/app.component.ts
@@ -1,7 +1,20 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app',
-    template: '<h1>My Apps</h1>'
+    selector: 'mw-app',
+    // template: `
+    //     <h1>My Apps</h1>
+    //     <p>Keeping track fo the media I want to watch</p>
+    // `
+    templateUrl: 'app/app.component.html',
+    // styles: [`
+    //     h1 { color: #fff; }
+    //     .description {
+    //         font-style: italic;
+    //         color: green;
+    //     }
+    // `]
+    styleUrls: ['app/app.component.css']
+
 })
 export class AppComponent {}
