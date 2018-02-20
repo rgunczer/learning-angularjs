342b7d546b210f6314e65c0311cce2da6bbb2567
NITRO15\flanker <rgunczer@gmail.com>
2018-02-20 21:46:29 +0100

    binding and event @Input @Output

--- angular/angular2-essential-training/app/app.component.ts
+++ angular/angular2-essential-training/app/app.component.ts
@@ -17,4 +17,20 @@ import { Component } from '@angular/core';
     styleUrls: ['app/app.component.css']
 
 })
-export class AppComponent {}
+export class AppComponent {
+
+    firstMediaItem = {
+        id: 1,
+        name: "Firebug",
+        medium: "Series",
+        category: "Science Fiction",
+        year: 2010,
+        watchedOn: 1294166565384,
+        isFavorite: false
+    };
+
+    onMediaItemDelete(mediaItem) {
+        console.log('onMediaItemDelete...');
+    }
+
+}
--- angular/angular2-essential-training/app/media-item.component.ts
+++ angular/angular2-essential-training/app/media-item.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
     selector: 'mw-media-item',
     templateUrl: 'app/media-item.component.html',
     styleUrls: ['app/media-item.component.css']
 })
-export class MediaItemComponent {}
+export class MediaItemComponent {
+    // name = 'The Redemption';
+    // wasWatched() {
+    //     return true;
+    // }
+    @Input() mediaItem;
+    // @Input('mediaItemToWatch') mediaItem;
+    @Output() delete = new EventEmitter();
+
+    onDelete() {
+        console.log('deleted');
+        this.delete.emit(this.mediaItem);
+    }
+}
