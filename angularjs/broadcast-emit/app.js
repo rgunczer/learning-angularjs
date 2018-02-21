746ac32461e8b3f6cae8b6591607e1a5624d8b03
NITRO15\flanker <rgunczer@gmail.com>
2018-02-21 22:03:41 +0100

    $broadcast and $emit

---
+++ angularjs/broadcast-emit/app.js
@@ -0,0 +1,21 @@
+angular.module('App', [])
+
+    .controller('myController', function ($scope, $log) {
+        $scope.text = 'myController';
+        $scope.$on('talkToParent', function(event, args) {
+            alert(event.name + ' ' + JSON.stringify(args));
+        });
+        $scope.onBroadcast = function() {
+            $scope.$broadcast('talkToChildren', { msg: 'message' });
+        };
+    })
+
+    .controller('myOtherController', function($scope, $log) {
+        $scope.text = 'myOtherController';
+        $scope.onEmit = function () {
+            $scope.$emit('talkToParent', { msg: 'myMessage' });
+        };
+        $scope.$on('talkToChildren', function(event, args) {
+            alert(event.name + ' ' + JSON.stringify(args));
+        });
+    });
