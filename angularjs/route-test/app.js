f83aa1d2c6d3630658a07918b0c49a1d2baa9ae3
NITRO15\flanker <rgunczer@gmail.com>
2018-02-25 22:04:03 +0100

    route test

---
+++ angularjs/route-test/app.js
@@ -0,0 +1,39 @@
+angular.module('App', ['ngRoute'])
+    .controller('myController', function($scope) {
+        $scope.text = 'Joel';
+    })
+    .factory('myAuth', function($location) {
+        return {
+            checkAccess: function() {
+                $location.path('/home');
+            }
+        };
+    })
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                title: 'root',
+                template: '<h1>root</h1>',
+                resolve: {
+                    check: function($timeout, $log, myAuth, $location) {
+                        return $timeout(function() {
+                            $log.info('root resolved');
+                            //myAuth.checkAccess();
+                            //$location.path('/home');
+                        }, 1000);
+                    }
+                }
+            })
+            .when('/home', {
+                title: 'home',
+                template: '<h1>home</h1>'
+            })
+            .when('/denied', {
+                template: '<h1>denied</h1>',
+                title: 'denied'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+
+    });
