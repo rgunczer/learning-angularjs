b54d4146cbac983135ce78bbd3fb3c2bfd5d153d
NITRO15\flanker <rgunczer@gmail.com>
2018-03-12 22:11:49 +0100

    protractor reporter

--- protractor/conf.js
+++ protractor/conf.js
@@ -1,3 +1,5 @@
+var Jasmine2HtmlReporter = require('protractor-jasmine2-html-reporter');
+
 exports.config = {
     directConnect: false,
     seleniumAddress: 'http://localhost:4444/wd/hub',
@@ -17,4 +19,11 @@ exports.config = {
         defaultTimeoutInterval: 30000
     },
     resultJsonOutputFile: 'result.json',
+    onPrepare: function() {
+        jasmine.getEnv().addReporter(
+            new Jasmine2HtmlReporter({
+                savePath: 'target/screenshots'
+            })
+        );
+    }
 };
