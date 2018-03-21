e0776d3572195afbcdc3211f1807b1c3c0fa076c
NITRO15\flanker <rgunczer@gmail.com>
2018-03-21 21:59:49 +0100

    more angular stuff

---
+++ angular/kudv_angular/19-custom-pipe/WebApp/src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { EmployeeListComponent } from './employee/employeeList.component';
+import { EmployeeTitlePipe } from './employee/employeeTitle.pipe';
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule ],
+    declarations: [AppComponent, EmployeeComponent, EmployeeListComponent, EmployeeTitlePipe ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
