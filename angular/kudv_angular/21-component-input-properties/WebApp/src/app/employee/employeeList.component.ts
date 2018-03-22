6e2d87377ccb7930c657142437690fa20c05d6a6
NITRO15\flanker <rgunczer@gmail.com>
2018-03-22 21:44:10 +0100

    interfaces, input / output props, ng-container, nested components

---
+++ angular/kudv_angular/21-component-input-properties/WebApp/src/app/employee/employeeList.component.ts
@@ -0,0 +1,71 @@
+﻿import { Component } from '@angular/core';
+
+
+@Component({
+    selector: 'list-employee',
+    templateUrl: 'app/employee/employeeList.component.html',
+    styleUrls: ['app/employee/employeeList.component.css']
+})
+export class EmployeeListComponent {
+    employees: any[];
+    
+    constructor() {
+        this.employees = [
+            {
+                code: 'emp101',
+                name: 'Tom',
+                gender: 'Male',
+                annualSalary: 5500,
+                dateOfBirth: '6/25/1988'
+            },
+            {
+                code: 'emp102',
+                name: 'Alex',
+                gender: 'Male',
+                annualSalary: 5700.95,
+                dateOfBirth: '9/6/1982'
+            },
+            {
+                code: 'emp103',
+                name: 'Mike',
+                gender: 'Male',
+                annualSalary: 5900,
+                dateOfBirth: '12/8/1979'
+            },
+            {
+                code: 'emp104',
+                name: 'Mary',
+                gender: 'Female',
+                annualSalary: 6500.826,
+                dateOfBirth: '10/14/1980'
+            },
+            {
+                code: 'emp105',
+                name: 'Nancy',
+                gender: 'Female',
+                annualSalary: 6700.826,
+                dateOfBirth: '12/15/1982'
+            },
+            {
+                code: 'emp106',
+                name: 'Steve',
+                gender: 'Male',
+                annualSalary: 7700.481,
+                dateOfBirth: '11/18/1979'
+            }
+        ];
+    }
+
+    getTotalEmployeesCount() : number {
+        return this.employees.length;
+    }
+
+    getTotalMaleEmployeesCount(): number {
+        return this.employees.filter(e => e.gender === 'Male').length;
+    }
+
+    getTotalFemaleEmployeesCount(): number {
+        return this.employees.filter(e => e.gender === 'Female').length;
+    }
+
+}
