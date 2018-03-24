0517c90f573e7a0f3e4df83480995b25c980e6a1
NITRO15\flanker <rgunczer@gmail.com>
2018-03-24 21:34:17 +0100

    services

---
+++ angular/kudv_angular/25-services/WebApp/src/app/employee/employee.service.js
@@ -0,0 +1,65 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var core_1 = require("@angular/core");
+var EmployeeService = /** @class */ (function () {
+    function EmployeeService() {
+    }
+    EmployeeService.prototype.getEmployees = function () {
+        return [
+            {
+                code: 'emp101',
+                name: 'Tom',
+                gender: 'Male',
+                annualSalary: 5500,
+                dateOfBirth: '6/25/1988'
+            },
+            {
+                code: 'emp102',
+                name: 'Alex',
+                gender: 'Male',
+                annualSalary: 5700.95,
+                dateOfBirth: '9/6/1982'
+            },
+            {
+                code: 'emp103',
+                name: 'Mike',
+                gender: 'Male',
+                annualSalary: 5900,
+                dateOfBirth: '12/8/1979'
+            },
+            {
+                code: 'emp104',
+                name: 'Mary',
+                gender: 'Female',
+                annualSalary: 6500.826,
+                dateOfBirth: '10/14/1980'
+            },
+            {
+                code: 'emp105',
+                name: 'Nancy',
+                gender: 'Female',
+                annualSalary: 6700.826,
+                dateOfBirth: '12/15/1982'
+            },
+            {
+                code: 'emp106',
+                name: 'Steve',
+                gender: 'Male',
+                annualSalary: 7700.481,
+                dateOfBirth: '11/18/1979'
+            }
+        ];
+    };
+    EmployeeService = __decorate([
+        core_1.Injectable()
+    ], EmployeeService);
+    return EmployeeService;
+}());
+exports.EmployeeService = EmployeeService;
+//# sourceMappingURL=employee.service.js.map
