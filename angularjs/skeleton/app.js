5868ccd8ec0e72be4a638bcd0a6562febb96ad95
NITRO15\flanker <rgunczer@gmail.com>
2018-03-25 22:01:34 +0200

    remove npm command from js file

--- angularjs/skeleton/app.js
+++ angularjs/skeleton/app.js
@@ -4,5 +4,3 @@ angular.module('App', [])
         $scope.text = 'If you can read this then AngularJS is working fine';
         $log.info('hello from myController');
     });
-
-    npm install --save-dev @types/angular@1.5.11
