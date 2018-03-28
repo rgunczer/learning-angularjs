df8c97f0f3fbed51f01743732d759377316366dd
NITRO15\flanker <rgunczer@gmail.com>
2018-03-28 21:54:36 +0200

    route params

---
+++ angular/kudv_angular/31-route-params/WebApp/src/app/employee/employeeList.component.js
@@ -0,0 +1,55 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var core_1 = require("@angular/core");
+var employee_service_1 = require("./employee.service");
+var EmployeeListComponent = /** @class */ (function () {
+    function EmployeeListComponent(_employeeService) {
+        this._employeeService = _employeeService;
+        this.selectedEmployeeCountRadioButton = 'All';
+        this.statusMessage = 'Loading data.Please wait...';
+    }
+    EmployeeListComponent.prototype.onEmployeeCountRadioButtonChange = function (selectedRadioButtonValue) {
+        this.selectedEmployeeCountRadioButton = selectedRadioButtonValue;
+    };
+    EmployeeListComponent.prototype.ngOnInit = function () {
+        var _this = this;
+        console.log('ngOnInit...');
+        this._employeeService.getEmployees()
+            .subscribe(function (employeeData) {
+            _this.employees = employeeData;
+        }, function (error) {
+            _this.statusMessage = 'Problem with the service. Please try again later.';
+            console.error(error);
+        });
+    };
+    EmployeeListComponent.prototype.getTotalEmployeesCount = function () {
+        return this.employees.length;
+    };
+    EmployeeListComponent.prototype.getTotalMaleEmployeesCount = function () {
+        return this.employees.filter(function (e) { return e.gender === 'Male'; }).length;
+    };
+    EmployeeListComponent.prototype.getTotalFemaleEmployeesCount = function () {
+        return this.employees.filter(function (e) { return e.gender === 'Female'; }).length;
+    };
+    EmployeeListComponent = __decorate([
+        core_1.Component({
+            selector: 'list-employee',
+            templateUrl: 'app/employee/employeeList.component.html',
+            styleUrls: ['app/employee/employeeList.component.css']
+            //providers: [EmployeeService]
+        }),
+        __metadata("design:paramtypes", [employee_service_1.EmployeeService])
+    ], EmployeeListComponent);
+    return EmployeeListComponent;
+}());
+exports.EmployeeListComponent = EmployeeListComponent;
+//# sourceMappingURL=employeeList.component.js.map
