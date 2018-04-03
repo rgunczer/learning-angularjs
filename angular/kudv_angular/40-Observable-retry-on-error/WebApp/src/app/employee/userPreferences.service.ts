b7c1e100e1e836b300cf84b7a7fe5025ea86f256
NITRO15\flanker <rgunczer@gmail.com>
2018-04-03 21:58:10 +0200

    Observable retry on error

---
+++ angular/kudv_angular/40-Observable-retry-on-error/WebApp/src/app/employee/userPreferences.service.ts
@@ -0,0 +1,9 @@
+﻿import { Injectable } from '@angular/core';
+
+@Injectable()
+export class UserPreferencesService {
+    constructor() {
+        console.log('UserPreferencesService ctor');
+    }
+    colorPreference: string = 'orange';
+}
---
+++ angular/kudv_angular/40-Observable-retry-on-error/WebApp/src/app/others/pageNotFound.component.ts
@@ -0,0 +1,8 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    template: '<h1>The page you are looking from does not exist</h1>'
+})
+export class PageNotFoundComponent {
+
+}
