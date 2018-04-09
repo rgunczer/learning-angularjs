a5187b9106db61063efe6d28bc1a7ff4b38bedb1
NITRO15\flanker <rgunczer@gmail.com>
2018-04-09 21:39:51 +0200

    angular sandbox

---
+++ angular/sandbox/simple/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { SimpleComponent } from './simple/simple.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SimpleComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
