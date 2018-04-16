4b11d52ed0f824ec4c231915c138a018b478af7e
NITRO15\flanker <rgunczer@gmail.com>
2018-04-16 21:53:51 +0200

    change log message to something more meaningful

--- angularjs/manual-bootstrap/app.js
+++ angularjs/manual-bootstrap/app.js
@@ -8,6 +8,6 @@ angular.module('App', [])
 angular
     .element(document)
     .ready(function() {
-        console.log('here');
+        console.log('before angularjs bootstrap');
         angular.bootstrap(document, ['App']);
     });
