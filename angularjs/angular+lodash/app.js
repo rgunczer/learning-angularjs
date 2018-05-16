6af1a54eb9d9001539459703ebe176eb3d5cb11c
NITRO15\flanker <rgunczer@gmail.com>
2018-05-16 22:05:34 +0200

    angularjs + lodash basic sample

---
+++ angularjs/angular+lodash/app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('App', [])
+
+    .controller('myController', function ($scope, $log) {
+        $scope.text = 'If you can read this then AngularJS is working fine';
+        $log.info('hello from myController');
+
+        console.log(_.VERSION);
+    });
