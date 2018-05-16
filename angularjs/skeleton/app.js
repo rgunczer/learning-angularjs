a58fe8c714e1ac4baae822e21594227d321646c8
NITRO15\flanker <rgunczer@gmail.com>
2018-05-16 22:05:03 +0200

    use strict and add lodash

--- angularjs/skeleton/app.js
+++ angularjs/skeleton/app.js
@@ -1,7 +1,8 @@
+'use strict';
+
 angular.module('App', [])
 
     .controller('myController', function ($scope, $log) {
         $scope.text = 'If you can read this then AngularJS is working fine';
         $log.info('hello from myController');
     });
-
