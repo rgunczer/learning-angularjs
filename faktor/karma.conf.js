175d2bb51fbd943a63cb4092f7eae28f0a2377d2
NITRO15\flanker <rgunczer@gmail.com>
2018-05-24 22:17:29 +0200

    disable code coverage to be able to debug from karma

--- faktor/karma.conf.js
+++ faktor/karma.conf.js
@@ -15,6 +15,7 @@ module.exports = function (config) {
 
         // list of files / patterns to load in the browser
         files: [
+            'SpecHelper.js',
             'lib/angular-1.6.9/angular.js',
             'lib/angular-1.6.9/angular-mocks.js',
             'src/htmls.module.js',
@@ -32,7 +33,7 @@ module.exports = function (config) {
         // preprocess matching files before serving them to the browser
         // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
         preprocessors: {
-            'src/**/!(*spec).js': ['coverage']
+            //'src/**/!(*spec).js': ['coverage']
         },
 
 
