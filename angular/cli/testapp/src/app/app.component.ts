b30f014cf776524180aeb339ead9a3eda97ad9bc
NITRO15\flanker <rgunczer@gmail.com>
2018-06-11 23:33:55 +0200

    simple ng-bootstrap modal test with angular

---
+++ angular/cli/testapp/src/app/app.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+    title = 'my app title';
+    closeResult: string;
+
+    constructor(private modalService: NgbModal) { }
+
+    showSimpleAlert(): void {
+        alert('some simple alert to be show here');
+    }
+
+    openModal(content): void {
+        this.modalService.open(content).result.then((result) => {
+            this.closeResult = `Closed with: ${result}`;
+        }, (reason) => {
+            this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+        });
+    }
+
+    private getDismissReason(reason: any): string {
+        if (reason === ModalDismissReasons.ESC) {
+            return 'by pressing ESC';
+        } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+            return 'by clicking on a backdrop';
+        } else {
+            return `with: ${reason}`;
+        }
+    }
+
+}
