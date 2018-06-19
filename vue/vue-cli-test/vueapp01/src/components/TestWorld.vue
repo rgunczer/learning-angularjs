7b39b3c0dbd68daa4eda0b2daf92acebe2c86a0d
NITRO15\flanker <rgunczer@gmail.com>
2018-06-19 23:00:55 +0200

    vue test app with router and devexpress grid works in ie11

---
+++ vue/vue-cli-test/vueapp01/src/components/TestWorld.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <h1>Some Test {{stuff}} stuff goes here</h1>
+        <button v-on:click="sayHello">Say Hello</button>
+        <button v-on:click="counter++">Inc</button>
+        <button v-on:click="counter--">Dec</button>
+        <p>
+            {{ counter }}
+            <span v-if="counter === 0"> it's Zero</span>
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TestWorld',
+    data() {
+        return {
+            stuff: 'WORLD',
+            counter: 0
+        }
+    },
+    methods: {
+        sayHello() {
+            alert('Hello from button click');
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
