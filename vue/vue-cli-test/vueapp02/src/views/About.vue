2b74e728702e031749cefa91a6a8656c4bde2622
NITRO15\flanker <rgunczer@gmail.com>
2018-06-21 23:35:34 +0200

    vue cli-3 generated app

---
+++ vue/vue-cli-test/vueapp02/src/views/About.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>This is an about page, right!?</h1>
+  </div>
+</template>
---
+++ vue/vue-cli-test/vueapp02/vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  productionSourceMap: true,
+  baseUrl: undefined,
+  outputDir: undefined,
+  assetsDir: undefined,
+  runtimeCompiler: undefined,
+  parallel: undefined,
+  css: undefined,
+  sourceMaps: true
+}
