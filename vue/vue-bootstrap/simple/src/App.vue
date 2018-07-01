f08bed0191bfc629d2ae696b4c2abf5454da05bc
NITRO15\flanker <rgunczer@gmail.com>
2018-07-01 22:58:28 +0200

    vue modal bootstrap test

---
+++ vue/vue-bootstrap/simple/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <hr>
+    <Test @openDialog="openDlg" />
+    <b-modal ref="myModalRef" id="modal2" title="Bootstrap-Vue">
+      <p>Hello from modal on App.vue</p>
+    </b-modal>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import Test from "./components/Test";
+
+export default {
+  name: "app",
+  methods: {
+    openDlg() {
+      this.$refs.myModalRef.show()
+    }
+  },
+  components: {
+    HelloWorld,
+    Test
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue/vue-bootstrap/simple/src/components/Test.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <b-button @click="openDialog">I am a Button</b-button>
+        <b-button v-b-modal.modal1 variant="primary">I am a Primary Button</b-button>
+        <b-modal id="modal1" title="Bootstrap-Vue">
+            <p>Hello from modal</p>
+        </b-modal>
+
+        <button @click="modalShow = !modalShow">sHow</button>
+        <b-modal title="Bootstrap-Vue-Modal" v-model="modalShow">
+            <p>Hello from modal using modalShow</p>
+        </b-modal>
+
+    </div>
+</template>
+
+
+<script>
+export default {
+    data() {
+        return {
+            modalShow: false
+        }
+    },
+    methods: {
+        openDialog() {
+            this.$emit('openDialog');
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
