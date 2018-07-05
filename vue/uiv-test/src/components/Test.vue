5dd45177633d4429d0553a2f8cf2a8fc8e9dfb77
NITRO15\flanker <rgunczer@gmail.com>
2018-07-05 23:14:54 +0200

    dynamic login dialog component

--- vue/uiv-test/src/components/Test.vue
+++ vue/uiv-test/src/components/Test.vue
@@ -2,32 +2,38 @@
     <div>
         <p>Hello there: {{ msg }}</p>
         <hr>
-        <button class="btn btn-primary" @click="showModal1 = !showModal1">Open Modal 1</button>
+        <button class="btn btn-primary" @click="toggleModal">Open Modal 1</button>
         <hr>
-        {{ showModal }}
-        <modal v-movable
-            title="Modal 1"
-            v-model="showModal1"
-        >
+        <!-- <modal v-movable title="Modal 1" v-model="showModal1">
             <h4>Text in modal</h4>
-            <!-- <btn type="info" @click="showModal2 = !showModal2">Open Modal 2</btn> -->
-            <button type="button" class="btn btn-primary" @click="showModal2 = !showModal2">Open Modal 2</button>
-        </modal>
-        <modal v-movable v-model="showModal2" title="Modal 2">
-            <p>This is a nested modal</p>
-        </modal>
+            <btn type="info" @click="showModal2 = !showModal2">Open Modal 2</btn> -->
+            <!-- <button type="button" class="btn btn-primary" @click="showModal2 = !showModal2">Open Modal 2</button> -->
+        <!-- </modal> -->
+        <login @login="handleLogin" v-if="showModal1"/>
+        <p v-if="showModal1">now you see me</p>
     </div>
 </template>
 
 <script>
+import Login from './Login.vue'
+
 export default {
     props: ['msg'],
     data() {
         return {
-            showModal1: false,
-            showModal2: false,
-            showModal3: false
+            showModal1: false
         }
+    },
+    methods: {
+        handleLogin (payload) {
+            console.log('handle login here...' + JSON.stringify(payload));
+        },
+        toggleModal () {
+            this.showModal1 = !this.showModal1
+        }
+    },
+    components: {
+        Login
     }
 }
 </script>
