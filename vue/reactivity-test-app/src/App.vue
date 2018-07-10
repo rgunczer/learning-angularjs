441f5b5e943e4133fd987a505b0ef305e987cd7c
NITRO15\flanker <rgunczer@gmail.com>
2018-07-10 23:10:56 +0200

    reactivity test app

---
+++ vue/reactivity-test-app/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <button @click="addRandomFruit">Add Random Fruit</button>
+    <!-- <FruitStore msg="Welcome to Your Vue.js App" :fruits="fruitsList"/> -->
+    <FruitStore msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import _ from 'lodash';
+
+import FruitStore from './components/FruitStore.vue';
+
+export default {
+  name: 'app',
+  // computed: {
+  //   fruitsList() {
+  //     return this.$store.getters.fruits;
+  //   },
+  // },
+  methods: {
+    addRandomFruit() {
+      console.log('add random fruit');
+      const randomId = _.uniqueId();
+      const obj = { id: randomId, name: 'apple-' + randomId, selected: false };
+      // console.log(`obj is: ${JSON.stringify(obj)}`);
+
+      this.$store.commit('addFruit', obj);
+    },
+  },
+  components: {
+    FruitStore,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue/reactivity-test-app/src/components/FruitPanel.vue
@@ -0,0 +1,18 @@
+<template>
+    <div style="background-color: grey; broder: 1px solid black; padding-top: 10px; min-height:30px;" @click="toggleSelection(fruit)">
+        <div>{{fruit.name}}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: [ 'fruit' ],
+    methods: {
+        toggleSelection(fruit) {
+            console.log(`toggle selection: ${fruit.id}`);
+            this.$store.commit('setSelected', { id: fruit.id });
+            // this.$forceUpdate()
+        },
+    }
+}
+</script>
