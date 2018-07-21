fef34906d95cc4f860a01da76c75595c3eaae83c
NITRO15\flanker <rgunczer@gmail.com>
2018-07-22 00:01:54 +0200

    vue custom modal

---
+++ vue/vue-bootstrap/simple/src/components/CustomModalConent.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+    <p>here is a custom modal content [{{ name }}]</p>
+    <button class="btn btn-primary" @click="clickMe">Click Me</button>
+</div>
+</template>
+
+<script>
+export default {
+    props: ['name'],
+    methods: {
+        clickMe() {
+            this.$emit('mojo')
+        }
+    }
+}
+</script>
