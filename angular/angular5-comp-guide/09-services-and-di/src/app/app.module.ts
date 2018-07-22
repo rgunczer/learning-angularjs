69e13e92b9c503e7de7d6eb9003c2e1801be8876
NITRO15\flanker <rgunczer@gmail.com>
2018-07-22 23:31:10 +0200

    data service

--- angular/angular5-comp-guide/09-services-and-di/src/app/app.module.ts
+++ angular/angular5-comp-guide/09-services-and-di/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { AccountComponent } from './account/account.component';
 import { NewAccountComponent } from './new-account/new-account.component';
-// import { LoggingService } from './logging.service';
+import { LoggingService } from './logging.service';
 
 @NgModule({
   declarations: [
@@ -20,7 +20,7 @@ import { NewAccountComponent } from './new-account/new-account.component';
     HttpModule
   ],
   providers: [
-    // LoggingService
+    LoggingService
   ],
   bootstrap: [AppComponent]
 })
