7131c2e06b12a4450f752b7b55eed44f5a1d78d8
NITRO15\flanker <rgunczer@gmail.com>
2018-08-04 20:12:34 +0200

    rxjs6 example

---
+++ rxjs/rxjs6-test/app.js
@@ -0,0 +1,13 @@
+
+import { Observable, Subject, ReplaySubject, from, of, range } from 'rxjs';
+import { map, filter, switchMap } from 'rxjs/operators';
+
+range(1, 200)
+    // .pipe(filter(x => x % 2 === 1), map(x => x + x))
+    .subscribe(x => console.log(x));
+
+    // const app = (a,b) => {
+    //     return a + b;
+    // }
+
+    // alert(app(1,2));
