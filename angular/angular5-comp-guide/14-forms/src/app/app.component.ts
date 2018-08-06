bd976eaf1d2550dc7b6ff248ea61e724b9b0f674
NITRO15\flanker <rgunczer@gmail.com>
2018-08-06 23:18:22 +0200

    angular TD form handling

---
+++ angular/angular5-comp-guide/14-forms/src/app/app.component.ts
@@ -0,0 +1,66 @@
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  @ViewChild('f') signupForm: NgForm;
+  // defaultQuestion = 'pet';
+  defaultQuestion = 'teacher';
+  answer = '';
+  genders = [ 'male', 'female' ];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+  submitted = false;
+
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   questionsAnswer: '',
+    //   gender: 'male'
+    // });
+
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
+  }
+
+  // onSubmit(form: HTMLFormElement) {
+  //   console.log('submitted...');
+  //   console.log(form);
+  // }
+
+  // onSubmit(form: NgForm) {
+  //   console.log('submitted...');
+  //   console.log(form);
+  // }
+
+  onSubmit() {
+    this.submitted = true;
+    console.log('submitted...');
+    console.log(this.signupForm);
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionsAnswer;
+    this.user.gender = this.signupForm.value.gender;
+
+    this.signupForm.reset();
+  }
+
+}
