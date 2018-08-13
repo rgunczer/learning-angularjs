0dc64f1b85e023f612193ed1bd180ba0d83c655b
NITRO15\flanker <rgunczer@gmail.com>
2018-08-14 00:29:42 +0200

    learning

---
+++ learn/src/data.js
@@ -0,0 +1,7 @@
+export const villains = ['Pagan Min', 'Trevor Philips', 'Letho', 'Joker'];
+export const heroes = [ 'Batman', 'Spiderman' ];
+export const Joel = {
+    name: 'Joel',
+    age: 54,
+    weapon: 'fist'
+};
---
+++ learn/src/utils.js
@@ -0,0 +1,17 @@
+import { villains as names } from './data';
+
+export const SerialKiller = {
+    name: "Freddy Kruger",
+    age: 45,
+    killed: 34
+}
+
+export function sayMyName(obj) {
+    console.log(`sayMyName1: ${obj.name}`);
+    console.log(`sayMyName2: ${obj.name}`);
+    console.log(`sayMyName3: ${obj.name}`);
+}
+
+export function sayNames() {
+    names.forEach(name => console.log(name));
+}
