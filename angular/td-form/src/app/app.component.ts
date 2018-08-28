75696e69dc0b5a7c43f2c19b78d3763950c942a8
NITRO15\flanker <rgunczer@gmail.com>
2018-08-29 01:11:03 +0200

    td form more tests

--- angular/td-form/src/app/app.component.ts
+++ angular/td-form/src/app/app.component.ts
@@ -7,11 +7,40 @@ import { NgForm } from '@angular/forms';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+    defaultAge = 2;
+    game = '';
+    colors = ['red', 'green', 'blue'];
+
+
     @ViewChild('f') signupForm: NgForm;
 
     // onSubmit(form: NgForm) {
     //     console.log(form);
     // }
+
+    onAgeChange(age: string) {
+        console.log('on change... ' + age);
+        switch (age) {
+            case '1': this.game = 'tetris'; break;
+            case '2': this.game = 'pokemon'; break;
+            case '3': this.game = 'farcry'; break;
+        }
+    }
+
+    fillWithDefaults() {
+        this.signupForm.form.setValue({
+            name: 'Joel',
+            age: 1,
+            game: 'tetris',
+            color: 'green'
+        });
+    }
+
+    fillName() {
+        this.signupForm.form.patchValue({
+            name: 'xxx'
+        });
+    }
 
     onSubmit() {
         console.log(this.signupForm);
