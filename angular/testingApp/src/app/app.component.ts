5d5d5354b29a6d283a0e1bdbefc1153e2da7746f
NITRO15\flanker <rgunczer@gmail.com>
2018-09-20 22:58:26 +0200

    rxjs6 delay

--- angular/testingApp/src/app/app.component.ts
+++ angular/testingApp/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonService } from './common.service';
+import { of, from, Observable } from 'rxjs';
+import { delay, concatMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +11,7 @@ import { CommonService } from './common.service';
 export class AppComponent implements OnInit {
   bgColor = 'red';
   title = 'jonas';
+  numbers$: Observable<number>;
 
   constructor(
     private commonService: CommonService) {
@@ -16,10 +19,17 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     console.log(this.commonService.name);
+    this.numbers$ = from([1, 2, 3, 4, 5, 6, 7]).pipe(concatMap(num => of(num).pipe(delay(1000))));
+    // from([1, 2, 3, 4, 5, 6, 7]).pipe(concatMap(num => of(num).pipe(delay(1000))))
+    //   .subscribe(x => console.log(x));
   }
 
   onClick() {
     this.bgColor = 'yellow';
     // this.commonService.name = 'jamie';
+  }
+
+  testOf() {
+    this.numbers$.subscribe((x) => { console.log(x); });
   }
 }
