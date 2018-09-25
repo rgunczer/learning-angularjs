05c1455ca655c11717ea9370e6091b50b291a441
NITRO15\flanker <rgunczer@gmail.com>
2018-09-25 23:04:21 +0200

    forkJoin test

--- angular/rxApp/src/app/app.component.ts
+++ angular/rxApp/src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { of, forkJoin } from 'rxjs';
+import { map } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'rxApp';
+
+  constructor(
+    private http: HttpClient,
+    private titleService: Title
+  ) {
+
+  }
+
+  ngOnInit() {
+    console.log('ngOnInit...');
+
+    const title = this.titleService.getTitle();
+    console.log('title:', title);
+
+
+    const obs = of(1, 2, 3);
+
+    forkJoin(obs).subscribe((x) => console.log(x));
+
+    // const joint = obs.pipe(
+    //   map(x => {
+    //     // console.log(x);
+    //     return x;
+    //   })
+    // );
+
+    // joint.subscribe(
+    //   (values) => {
+    //     console.log('values: ', values);
+    //   }
+    // );
+  }
+
+  setTitle() {
+    this.titleService.setTitle('jancsi');
+  }
+
 }
--- angular/rxApp/src/app/app.module.ts
+++ angular/rxApp/src/app/app.module.ts
@@ -4,13 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { SimpleComponent } from './simple/simple.component';
 
+import { HttpClientModule  } from '@angular/common/http';
+
 @NgModule({
   declarations: [
     AppComponent,
     SimpleComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
