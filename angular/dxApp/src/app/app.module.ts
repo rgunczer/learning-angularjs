65817ee429c4dd6a14600d25479000f2b5635d79
rgunczer <rgunczer@gmail.com>
2018-11-09 22:37:03 +0100

    devextreme test app

---
+++ angular/dxApp/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { DxButtonModule } from 'devextreme-angular';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    DxButtonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
