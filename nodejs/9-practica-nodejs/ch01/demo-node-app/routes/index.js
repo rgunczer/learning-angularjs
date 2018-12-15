0640b1276f0ed8374e37cd5f9ba4a241186dd754
rgunczer <rgunczer@gmail.com>
2018-12-15 21:54:14 +0100

    finished app, listing registrations and styled with basic authorization

--- nodejs/9-practica-nodejs/ch01/demo-node-app/routes/index.js
+++ nodejs/9-practica-nodejs/ch01/demo-node-app/routes/index.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
+const auth = require('http-auth');
+const mongoose = require('mongoose');
 const { body, validationResult } = require('express-validator/check');
+
+const Registration = mongoose.model('Registration');
+
+const basic = auth.basic({
+    file: path.join(__dirname, '../users.htpasswd')
+});
 
 router.get('/', (req, res) => {
     res.render('form', { title: 'Registration form' });
@@ -21,7 +30,12 @@ router.post(
         const errors = validationResult(req);
 
         if (errors.isEmpty()) {
-            res.send('Thank you for your registration!')
+            const registration = new Registration(req.body);
+            registration.save()
+                .then(() => { res.send('Thank you for your registration!') })
+                .catch((err) => {
+                    res.send('Sorry! Something went wrong.' + err.message)
+                })
         } else {
             res.render('form', {
                 title: 'Registration form',
@@ -31,5 +45,13 @@ router.post(
         }
     }
 );
+
+router.get('/registrations', auth.connect(basic), (req, res) => {
+    Registration.find()
+        .then((registrations) => {
+            res.render('index', { title: 'Listing registrations', registrations });
+        })
+        .catch(() => { res.send('Sorry! Something went wrong.'); });
+});
 
 module.exports = router;
