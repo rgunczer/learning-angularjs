62d5ba92c07f99ce3788f67b27176553a11b497a
rgunczer <rgunczer@gmail.com>
2018-12-24 19:46:49 +0100

    inquirer test

---
+++ nodejs/simple-inquirer/index.js
@@ -0,0 +1,53 @@
+var inquirer = require('inquirer');
+
+var questions = [
+    // {
+    //     type: 'confirm',
+    //     name: 'toBeDelivered',
+    //     message: 'Is this for delivery?',
+    //     default: false
+    // },
+    {
+        type: 'input',
+        name: 'component-name',
+        message: 'Component Name'
+    },
+    {
+        type: 'list',
+        name: 'size',
+        message: 'What size?',
+        choices: ['Large', 'Medium', 'Small'],
+        filter: (val) => {
+            return val.toLocaleLowerCase();
+        }
+    },
+    {
+        type: 'checkbox',
+        name: 'support',
+        message: 'Select Support',
+        choices: [
+            {
+                name: 'Grid'
+            },
+            {
+                name: 'Modal'
+            },
+            new inquirer.Separator(' ---- '),
+            {
+                name: 'UserPreferences'
+            }
+        ]
+    },
+    {
+        type: 'input',
+        name: 'comments',
+        message: 'Any Comments?',
+        default: 'Nope. All good!'
+    }
+]
+
+inquirer
+    .prompt(questions)
+    .then(answers => {
+        console.log(answers);
+    });
