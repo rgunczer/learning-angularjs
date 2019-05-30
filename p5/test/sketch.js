4023c90cc9a1591e2f36f6275b3898babcb6b9b1
rgunczer <rgunczer@gmail.com>
2019-05-30 23:21:13 +0200

    ray casting

--- p5/test/sketch.js
+++ p5/test/sketch.js
@@ -1,41 +1,32 @@
-const cw = 720;
-const ch = 400;
-let rectColor;
-let backColor;
-let x = 0;
-let y = 0;
-const w = 60;
-const h = 40;
-let x_step = 10;
-let y_step = 10;
+
+let wall;
+let ray;
 
 function setup() {
-  backColor = color(255, 255, 255);
-  rectColor = color(255, 0, 0);
-  createCanvas(cw, ch);
+  createCanvas(400, 400);
+  wall = new Boundary(300, 100, 330, 300);
+  ray = new Ray(100, 200);
 }
 
 function draw() {
-  background(255);
+  background(0);
+  wall.show();
+  ray.show();
 
-  x+=x_step;
-  y+=y_step;
+  ray.lookAt(mouseX, mouseY)
 
-  if ((x + w) > cw) {
-    x_step *= -1;
+  let pt = ray.cast(wall);
+  if (pt) {
+    fill(255);
+    ellipse(pt.x, pt.y, 8, 8);
+
+    // stroke(255);
+    // push();
+    // translate(ray.pos.x, ray.pos.y);
+    line(ray.pos.x, ray.pos.y, pt.x, pt.y);
+    // pop();
+
   }
-  if (x < 0) {
-    x_step *= -1;
-  }
 
-  if ((y + h) > ch) {
-    y_step *= -1;
-  }
-  if (y < 0) {
-    y_step *= -1;
-  }
-
-  fill(rectColor);
-
-  rect(x, y, w, h);
+  // console.log(pt);
 }
