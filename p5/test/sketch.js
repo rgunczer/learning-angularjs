c5d4b27b53e2c30c8a0d93a4a8f962e5662f2ce6
rgunczer <rgunczer@gmail.com>
2019-05-31 23:09:04 +0200

    add more line segments

--- p5/test/sketch.js
+++ p5/test/sketch.js
@@ -1,32 +1,30 @@
 
-let wall;
+
+const walls = [];
 let ray;
 
 function setup() {
   createCanvas(400, 400);
-  wall = new Boundary(300, 100, 330, 300);
+  walls.push(new Boundary(300, 100, 330, 300));
+  walls.push(new Boundary(100, 90, 200, 100));
+  walls.push(new Boundary(10, 10, 20, 100));
   ray = new Ray(100, 200);
 }
 
 function draw() {
   background(0);
-  wall.show();
   ray.show();
 
   ray.lookAt(mouseX, mouseY)
 
-  let pt = ray.cast(wall);
-  if (pt) {
-    fill(255);
-    ellipse(pt.x, pt.y, 8, 8);
+  walls.forEach((wall) => {
+    wall.show();
+    let pt = ray.cast(wall);
+    if (pt) {
+      fill(255);
+      ellipse(pt.x, pt.y, 8, 8);
+      line(ray.pos.x, ray.pos.y, pt.x, pt.y);
+    }
+  });
 
-    // stroke(255);
-    // push();
-    // translate(ray.pos.x, ray.pos.y);
-    line(ray.pos.x, ray.pos.y, pt.x, pt.y);
-    // pop();
-
-  }
-
-  // console.log(pt);
 }
