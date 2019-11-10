81a109ab6c72256ab3a35f4a40efeba01c5caa3c
gunczer <gunczer@aol.com>
2019-11-10 14:50:46 +0100

    more complete example

--- js/misc/ora-test.js
+++ js/misc/ora-test.js
@@ -1,13 +1,29 @@
 const ora = require('ora');
 
-const spinner = ora({
-    text: 'Loading unicorns',
-    // spinner: 'dots'
-    frames: ['-', '+', '-']
+let index = 0;
+const tasks = [
+    'Loading unicorns',
+    'Loading rainbows',
+    'Making unicorns',
+    'Compiling colors',
+    'Adjusting tones',
+    'Last tweaks'
+];
 
+const spinner = ora({
+    color: 'yellow',
+    text: tasks[0],
+    spinner: 'arrow3'
 }).start();
 
-setTimeout(() => {
-    spinner.color = 'blue';
-    spinner.text = 'Loading rainbows';
-}, 1000);
+const intervalId  = setInterval(() => {
+    spinner.text = tasks[index++];
+
+    if (index > tasks.length) {
+        clearInterval(intervalId);
+        spinner.stop();
+        console.log('done');
+    }
+
+}, 2000);
+
